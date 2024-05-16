968d82d39bfc9e23951cfec1320f74230f173947
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-05-16 22:46:31 +0530

    add code for exercise 1.7

---
+++ ch1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+}
