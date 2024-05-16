c341369f60e996632229815c7e4fa07173a7364e
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-05-16 23:05:59 +0530

    add code for copy and made a web-app for testing purpose

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-func main() {
+func main1() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
@@ -15,11 +16,28 @@ func main() {
 			os.Exit(1)
 		}
 		b, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
 		fmt.Printf("%s", b)
+	}
+}
+func main() {
+	//improvement on the above code
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
 	}
 }
