e4615a1c4ff1e20fb2e2575fe5c65b54f21784d8
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-05-24 18:15:15 +0530

    add array type

---
+++ ch4/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Currency int
+
+const (
+	USD Currency = iota
+	EUR
+	YEN
+	INR
+	PKI
+	CZH
+)
+
+func main() {
+	var q [3]int = [3]int{1, 2, 3}
+	r := [2]int{5, 6}
+	fmt.Println(q)
+	fmt.Println(r)
+	a := [2]int{1, 2}
+	b := [...]int{1, 2}
+	c := [2]int{1, 3}
+	fmt.Println(a == b, b == c, a == c)
+	d := [...]int{1, 3, 4}
+	fmt.Println(c == d)
+	//enums
+}
