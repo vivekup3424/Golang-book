c2301acadfde8fb29dc0100e9d0743eda9d4d4d0
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-05-31 00:18:58 +0530

    add main go

---
+++ todo-go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item" bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Buy groceries", Completed: false},
+	{ID: "2", Item: "Read a book", Completed: false},
+	{ID: "3", Item: "Write code", Completed: false},
+	{ID: "4", Item: "Go for a walk", Completed: false},
+	{ID: "5", Item: "Cook dinner", Completed: false},
+	{ID: "6", Item: "Clean the house", Completed: false},
+	{ID: "7", Item: "Call a friend", Completed: false},
+}
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		val, _ := json.Marshal(todos)
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(val)
+	})
+	http.ListenAndServe("localhost:8081", router)
+}
+
