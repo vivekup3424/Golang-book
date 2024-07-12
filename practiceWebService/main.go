75e38d1f06e79251b3629cfc5292d6e5b82b4499
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-12 11:00:37 +0530

    add basic sever configuration

--- practiceWebService/main.go
+++ practiceWebService/main.go
@@ -4,18 +4,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 	"web-service/handlers"
 )
 
 func main() {
 	logger := log.New(os.Stdout, "product-api:s", log.LstdFlags)
-	sm := http.NewServeMux()
-	//NOTE: registers a handler
-	sm.HandleFunc("/hello", handlers.NewHello(logger).ServeHTTP)
-	sm.HandleFunc("/goodbye", handlers.NewGoodbye(logger).ServeHTTP)
-	err := http.ListenAndServe("localhost:8080", sm)
-	if err != nil {
-		log.Fatal(err)
+	router := http.NewServeMux()
+	server := http.Server{
+		Addr:           "127.0.0.1:8080",
+		Handler:        router,
+		IdleTimeout:    120 * time.Second,
+		ReadTimeout:    1 * time.Second,
+		WriteTimeout:   1 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-
+	//NOTE: registers a handler
+	router.HandleFunc("/hello", handlers.NewHello(logger).ServeHTTP)
+	router.HandleFunc("/goodbye", handlers.NewGoodbye(logger).ServeHTTP)
+	err := server.ListenAndServe()
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
