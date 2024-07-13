8931304b2c63b46591b28c69d39122cad1771878
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-07-13 05:38:41 +0530

    add gorilla mux

--- practiceWebService/main.go
+++ practiceWebService/main.go
@@ -2,19 +2,51 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 	"web-service/handlers"
 )
 
+const version = "1.0.0"
+
+// For now, the only configuration settings will be the network port that we want the
+// server to listen on, and the name of the current operating environment for the
+// application (development, staging, production, etc.). We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+type Application struct {
+	config  config
+	eLogger *log.Logger
+	iLogger *log.Logger
+}
+
 func main() {
-	logger := log.New(os.Stdout, "product-api:s", log.LstdFlags)
+	//declare an instance of config
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgreSQL DSN")
+	flag.Parse()
+
+	//NOTE: loggers for writing messages to stdout
+	iLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	eLogger := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//NOTE: creates a new server
 	router := http.NewServeMux()
 	server := http.Server{
-		Addr:           "127.0.0.1:8080",
+		Addr:           "127.0.0.1:" + strconv.Itoa(cfg.port),
 		Handler:        router,
 		IdleTimeout:    120 * time.Second,
 		ReadTimeout:    1 * time.Second,
@@ -22,13 +54,13 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	//NOTE: registers a handler
-	router.Handle("/hello", handlers.NewHello(logger))
-	router.Handle("/goodbye", handlers.NewGoodbye(logger))
-	router.Handle("/products", handlers.NewProducts(logger))
+	router.Handle("/hello", handlers.NewHello(iLogger))
+	router.Handle("/goodbye", handlers.NewGoodbye(iLogger))
+	router.Handle("/products", handlers.NewProducts(iLogger))
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
-			logger.Fatal(err)
+			iLogger.Fatal(err)
 		}
 	}()
 	//NOTE: code for managing the shutdown of the server
@@ -38,11 +70,11 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 	s := <-sigChan
-	logger.Println("Received terminate, graceful shutdown", s)
+	iLogger.Println("Received terminate, graceful shutdown", s)
 	d := time.Now().Add(30 * time.Second)
 	timeoutContext, _ := context.WithDeadline(context.Background(), d)
 	err := server.Shutdown(timeoutContext)
 	if err != nil {
-		logger.Fatal(err)
+		eLogger.Fatal(err)
 	}
 }
